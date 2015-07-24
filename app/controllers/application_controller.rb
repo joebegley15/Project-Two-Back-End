4d5f7898a3c76bef69211932e743655b0a721c0a
joebegley15 <joebegley15@gmail.com>
2015-07-24 15:52:25 -0400

    I don't know what happened

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      @current_user = User.find_by token: token
+      token == token
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authenticate, only: [:login, :create]
+ # before_action :set_user, only: [:show, :update, :destroy]
+
+
+
+  def login
+    credentials = user_credentials
+    token = User.login(credentials[:email], credentials[:password])
+    if token
+      render json: { token: token }
+    else
+      head :unauthorized
+    end
+  end
+
+
 
   # GET /users
   # GET /users.json
@@ -47,7 +62,12 @@ class UsersController < ApplicationController
     head :no_content
   end
 
+
   private
+
+  def user_credentials
+    params.require(:credentials).permit(:email, :password, :password_confirmation)
+  end
 
     def set_user
       @user = User.find(params[:id])
